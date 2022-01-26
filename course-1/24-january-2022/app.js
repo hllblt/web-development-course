24483368d07600c9da550871ac78bdb74de99d97
Halil Bulut <halill.bulutt.1@gmailcom>
2022-01-26 19:50:12 +0100

    added lesson 12 and 13

---
+++ course-1/24-january-2022/app.js
@@ -0,0 +1,20 @@
+const vol = document.getElementById("vol");
+const spann = document.getElementById("spann")
+
+console.log(vol.value)
+
+function vol1(){
+    spann.innerHTML = vol.value
+}
+
+// const buttonn = document.getElementById("buttonn");
+// buttonn.addEventListener("onclick",()=>{
+//     // console.log(vol.value)
+//     console.log("hi");
+// })
+
+// vol.addEventListener("change",()=>{
+//     console.log(vol.value)
+//     spann.innerHTML = vol.value
+    
+// })
